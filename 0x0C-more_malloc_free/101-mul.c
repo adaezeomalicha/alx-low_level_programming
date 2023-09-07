b5d51b1f1c82aa6f5abded376bae803b552250a8
Adaezeomalicha <xter491@gmail.com>
2023-09-07 08:24:52 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,54 +1,59 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+/**
+ * _atoi - Converts a character array to an integer
+ * @s: character array to convert
+ * Return: returns integer from array, returns 0 if none found
+ */
+
+int _atoi(char *s)
+{
+int retval, i, neg;
+i = 0;
+retval = 0;
+neg = -1;
+while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+{
+if (s[i] == '-')
+neg *= -1;
+i++;
+}
+while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+retval = (retval * 10) - (s[i++] - '0');
+return (retval *neg);
+}
 
 /**
- * _realloc - reallocates a memory block using malloc and free
- *
- * @ptr: original pointer to be reallocated
- * @old_size: size of old memory allocation in bytes
- * @new_size: size of new memory allocation in bytes
- *
- * Return: a pointer to the new memory allocation or NULL if the process fails
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	int *nptr, *p;
-	unsigned int c;
 
-	if (old_size == new_size)
-	{
-		return (ptr);
-	}
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	p = ptr;
-	nptr = malloc(new_size);
-	if (nptr == NULL)
-	{
-		return (NULL);
-	}
-	if (ptr == NULL)
-	{
-		return (nptr);
-	}
-	if (new_size > old_size)
-	{
-		for (c = 0; c < old_size; c++)
-		{
-			nptr[c] = p[c];
-		}
-	}
-	else
-	{
-		for (c = 0; c < new_size; c++)
-		{
-			nptr[c] = p[c];
-		}
-	}
-	free(ptr);
-	return (nptr);
+int main(int argc, char *argv[])
+{
+int a, b, i, j;
+unsigned long mul;
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] > 57 || argv[i][j] < 48)
+{
+printf("Error\n");
+exit(98);
+}
+}
+}
+a = _atoi(argv[1]);
+b = _atoi(argv[2]);
+mul = a *b;
+printf("%lu\n", mul);
+return (0);
 }
