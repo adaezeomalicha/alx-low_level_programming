02dc81d27cefe22d661b763f30fa2aadb272dd0e
Adaezeomalicha <xter491@gmail.com>
2023-09-13 09:18:48 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,32 +2,38 @@
 #include <stdlib.h>
 
 /**
- * main - entry point
- * Description: program that prints its own opcodes in two digit hexadecimal
- * lowercase format
- * @argc: argument count
- * @argv: argument vectors
- * Return: 0
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int n, i;
-	int (*ptr)(int, char **);
+	char *opc = (char *) main;
+	int i, nbytes;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	n = atoi(argv[1]);
-	if (n < 0)
+
+	nbytes = atoi(argv[1]);
+
+	if (nbytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	ptr = &main;
-	for (i = 0; i < n; i++)
-		printf("%.2x ", *((unsigned char *)(ptr + i)));
+
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
+
 	printf("\n");
 	return (0);
 }
